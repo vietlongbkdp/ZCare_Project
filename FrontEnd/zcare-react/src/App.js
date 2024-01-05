344196325c16ee9c30845fde2daaad43bcc9dfd6
vietlongbkdp <vietlongbkdp@gmail.com>
2024-01-05 16:16:01 +0700

    no message

--- FrontEnd/zcare-react/src/App.js
+++ FrontEnd/zcare-react/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import DoctorInfoClinic from "./components/DoctorInfoClinic/DoctorInfoClinic";
 function App() {
   return (
     <>
         <Header/>
+        <DoctorInfoClinic/>
         <Footer/>
     </>
   );
