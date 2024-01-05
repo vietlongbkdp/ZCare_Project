956bfb4c6c41fda14ade5a0a37b475abf6ac227d
LeBaTuong <tuongleba1998@gmail.com>
2024-01-05 16:56:55 +0700

    đã sửa backend

---
+++ BackEnds/ZCare-spring-boot/src/main/java/com/cg/api/DoctorAPI.java
@@ -0,0 +1,27 @@
+package com.cg.api;
+
+import com.cg.model.Doctor;
+import com.cg.repository.IDoctorRepository;
+import com.cg.service.doctor.IDoctorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/doctor")
+@CrossOrigin(origins = "*")
+public class DoctorAPI {
+    @Autowired
+    private IDoctorRepository doctorService;
+    @GetMapping
+    public ResponseEntity<?> getDoctors(){
+        List<Doctor> doctorList = doctorService.findAll();
+        return new ResponseEntity<>(doctorList, HttpStatus.OK);
+    }
+}
---
+++ BackEnds/ZCare-spring-boot/src/main/java/com/cg/service/doctor/IDoctorService.java
@@ -0,0 +1,7 @@
+package com.cg.service.doctor;
+
+import com.cg.model.Doctor;
+import com.cg.service.IGeneralService;
+
+public interface IDoctorService extends IGeneralService<Doctor,Long> {
+}
