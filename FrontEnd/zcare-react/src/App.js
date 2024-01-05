f69d3374abbf8cadf662e9328c4953ae5cbaa086
LeBaTuong <tuongleba1998@gmail.com>
2024-01-05 16:33:30 +0700

    tuong123

--- FrontEnd/zcare-react/src/App.js
+++ FrontEnd/zcare-react/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import Header from "./components/Header/Header";
+
+import Project from "./components/Header/Header"
 
 function App() {
   return (
     <>
-      <Header/>
+      <Project/>
     </>
   );
 }
