a4259dc0307d4ec9636cf0123132a1617513984a
vietlongbkdp <vietlongbkdp@gmail.com>
2024-01-08 17:28:30 +0700

    update dashboard

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,15 +1,9 @@
 import './App.css';
-import Header from "./components/Header/Header";
-import DoctorInfoClinic from "./components/DoctorInfoClinic/DoctorInfoClinic";
-import Footer from "./components/Footer/Footer";
-import MapRender from "./components/MapRender/MapRender";
+import Dashboard from "./Dashboard";
 export default function App() {
     return (
         <>
-            <Header/>
-            <DoctorInfoClinic/>
-            <MapRender/>
-            <Footer/>
+            <Dashboard/>
         </>
     )
 }
