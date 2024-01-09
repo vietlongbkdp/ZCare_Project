5f6162d922c366267c6cfd1614736f3b2d467bb4
QuocDatHang <quocdathang1997@gmail.com>
2024-01-09 17:30:09 +0700

    init

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,23 +1,14 @@
 import './App.css';
-// import AdminCooperate from "./components/Cooperate/AdminCooperate";
-import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import MusculoskeletalInfo from "./components/Specialist/MusculoskeletalInfo";
-import NerveInfo from "./components/Specialist/NerveInfo";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-
-import MapRender from "./components/MapRender/MapRender";
 import App1 from "./components/Dashboard/AdminNav"
+
 export default function App() {
     return (
         <>
-            <App1/>
-            {/*<Header/>*/}
-            {/*<DoctorInfoClinic/>*/}
-            {/*<MapRender/>*/}
-            {/*<Footer/>*/}
-
+            <Header/>
+            <DoctorInfoClinic/>
+            <MapRender/>
+            <Footer/>
         </>
     )
 }
