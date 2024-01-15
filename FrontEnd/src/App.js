37cfa70638bf4d9d3afb2b5c15e562e0f1114e30
phuocvinh123 <doanvanphuocvinh1510@gmail.com>
2024-01-15 10:44:41 +0700

    chagnepassword

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -2,12 +2,13 @@ import "react-toastify/dist/ReactToastify.css";
 import HomePage from "./pages/Home/HomePage";
 import AuthLogin from "./components/Authentication/AuthLogin";
 import { Route, Routes } from "react-router-dom";
-import AuthRegister from './components/Authentication/AuthRegister';
-import ForgotPassword from './components/Authentication/ForgotPassword';
-import User from "./components/Doctor/TableDoctor"
-import Pagerbase from "./components/Dashboard/Paperbase"
-import DoctorInfor from "./components/Cooperate/AdminCooperate"
-import ClinicAdmin from "./components/ClinicAdmin/ClinicAdmin"
+import AuthRegister from "./components/Authentication/AuthRegister";
+import ForgotPassword from "./components/Authentication/ForgotPassword";
+import User from "./components/Doctor/TableDoctor";
+import Pagerbase from "./components/Dashboard/Paperbase";
+import DoctorInfor from "./components/Cooperate/AdminCooperate";
+import ClinicAdmin from "./components/ClinicAdmin/ClinicAdmin";
+import ChangePassword from "./components/Authentication/ChangePassword";
 
 export default function App() {
   return (
@@ -17,6 +18,7 @@ export default function App() {
         <Route path="/login" element={<AuthLogin />} />
         <Route path="/register" element={<AuthRegister />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/change-password" element={<ChangePassword />} />
         <Route path="/admin" element={<Pagerbase />}>
           <Route path="user" element={<User />}></Route>
           <Route path="doctorInfor" element={<DoctorInfor />}></Route>
