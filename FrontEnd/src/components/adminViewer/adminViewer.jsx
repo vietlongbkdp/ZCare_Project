08e18d17b5705a3d0fdb94b35fd87070ddf16185
vietlongbkdp <vietlongbkdp@gmail.com>
2024-01-26 13:52:18 +0700

    no message

---
+++ FrontEnd/src/components/adminViewer/adminViewer.jsx
@@ -0,0 +1,11 @@
+import Box from "@mui/material/Box";
+
+export default function AdminViewer(){
+    return (
+        <>
+        <Box>
+                hello
+        </Box>
+        </>
+    )
+}
