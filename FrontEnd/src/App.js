2a1171a044fadaf64f204652d370dde811b24083
QuocDatHang <quocdathang1997@gmail.com>
2024-02-19 13:57:39 +0700

    refactor code

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -41,6 +41,7 @@ import MedicalBookingList from "./components/ClinicAdmin/MedicalBookingList";
 import BookingListDoctor from "./components/Doctor/BookingListDoctor";
 import CustomerBookingAdmin from "./components/CustomerDashboard/CustomerBookingAdmin";
 import PaidBookingList from "./components/ClinicAdmin/PaidBookingList";
+import AdminCooperate from "./components/Cooperate/AdminCooperate";
 
 
 export default function App() {
@@ -59,7 +60,7 @@ export default function App() {
           <Route path="/admin" element={<Pagerbase />}>
             <Route path="" element={<AdminViewer />}></Route>
             <Route path="doctor" element={<DoctorAdmin API_URL={API_DOCTOR} />}></Route>
-            <Route path="doctorInfor" element={<DoctorInfor />}></Route>
+            <Route path="cooperate" element={<AdminCooperate />}></Route>
             <Route path="clinic" element={<ClinicAdmin />}></Route>
             <Route path="customer" element={<CustomerAdmin />}></Route>
             <Route path="bookingHistory" element={<CustomerBookingAdmin/>}></Route>
@@ -68,11 +69,9 @@ export default function App() {
         <Route element={<PrivateRouteClinicAdmin />}>
           <Route path="/clinicadmin" element={<Pagerbase />}>
             <Route path="" element={<DoctorListByClinicAdmin />} />
-            <Route path="list-clinic" element={<DoctorListByClinicAdmin />} />
-            <Route path="doctorInfor" element={<DoctorInfor/>}></Route>
-            <Route path="bookingHistory" element={<CustomerBookingAdminClinic/>}></Route>
             <Route path="booking" element={<MedicalBookingList/>}></Route>
             <Route path="paid" element={<PaidBookingList/>}></Route>
+            <Route path="bookingHistory" element={<CustomerBookingAdminClinic/>}></Route>
             <Route path="doctor" element={<DoctorInClinic />}></Route>
             <Route path="editClinic" element={<EditAdminClinic />}></Route>
             <Route path="customer" element={<CustomerInClinic />}></Route>
@@ -83,11 +82,9 @@ export default function App() {
         <Route element={<PrivateRouteDoctor redirectTo="/doctoradmin/doctorInfor"/>}>
           <Route path="/doctoradmin" element={<Pagerbase/>}>
             <Route path="" element={<DoctorByAdminClinic/>}></Route>
-            <Route path="doctorInfor" element={<DoctorByAdminClinic/>}></Route>
             <Route path="bookingHistory" element={<CustomerBookingDoctor/>}></Route>
             <Route path="doctorBooking" element={<BookingListDoctor/>}></Route>
             <Route path="clinic/:idCustomer/:idDoctor/:idBooking" element={<ResultTyping/>}></Route>
-            <Route path="doctorInfor1" element={<DoctorInfor/>}></Route>
           </Route>
         </Route>
         <Route path="/customer" element={<Pagerbase />}>
