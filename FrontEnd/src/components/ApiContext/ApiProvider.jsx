4b24a0c1dda83b6b8e194f916af96afd15d80ff3
vietlongbkdp <vietlongbkdp@gmail.com>
2024-03-08 09:18:35 +0700

    Merge branch 'main' of https://github.com/vietlongbkdp/ZCare_Project

--- FrontEnd/src/components/ApiContext/ApiProvider.jsx
+++ FrontEnd/src/components/ApiContext/ApiProvider.jsx
@@ -3,9 +3,9 @@ import React, { createContext } from 'react'
 export const ApiContext = createContext();
 export default function ApiProvider({children}) {
   const ALL_API_URL = {
-    API_DOCTOR: 'http://192.168.1.64:8080/api/doctor',
-    API:'http://192.168.1.64:8080'
-    // API:'http://localhost:8080'
+    API_DOCTOR: 'http://localhost:8080/api/doctor',
+    // API:'http://192.168.1.64:8080'
+    API:'http://localhost:8080'
   }
   return (
     <ApiContext.Provider value={ALL_API_URL}>
--- FrontEnd/src/components/utils/ApiUserLogin.jsx
+++ FrontEnd/src/components/utils/ApiUserLogin.jsx
@@ -18,7 +18,7 @@ export default function  UserProvider ({ children }) {
 
             const getUser = async () => {
                 try {
-                    const user1 = await axios.get(`http://192.168.1.64:8080 /api/user/finduser/${useremail}`);
+                    const user1 = await axios.get(`http://localhost:8080/api/user/finduser/${useremail}`);
                     // console.log(user1.data)
                     setUser(user1.data);
                     // console.log(user.id);
